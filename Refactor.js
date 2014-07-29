9030cf6d58c2769ed61d87aad6de5bbf639c56a2
ganara.jpermunda <ganaraj.permunda@lab49.com>
2014-07-29 17:14:47 +0100

    Refactor.

---
+++ Refactor.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ganara.jpermunda on 29/07/2014.
+ */
+
+/**
+ * We take in the AST for a function and the context in which it lives.
+ * Our task is to refactor the code in the function with a bunch of function calls.
+ *
+ *
+ * */
+module.exports = function(functionAST,context){
+
+};
