d3b77de2f0e85a44f0ec56e8bd619d6bc0aaa74f
ganara.jpermunda <ganaraj.permunda@lab49.com>
2014-07-31 17:59:27 +0100

    building the tree.

--- Refactor.js
+++ Refactor.js
@@ -4,23 +4,49 @@
 
 var esprima = require('esprima');
 var _ = require('lodash');
+
 /**
  * We take in the AST for a function and the context in which it lives.
  * Our task is to refactor the code in the function with a bunch of function calls.
  *
  *
  * */
-module.exports = function(functionAST,context){
 
+
+
+var rootContext;
+
+
+module.exports = function (functionAST, toContext) {
+
+    rootContext = toContext;
     var body = functionAST.ast.body.body;
-
     var ifs = getIfStatements(body);
-
+    processIfs(ifs);
 };
 
 
-function getIfStatements(expressions){
-    return _.filter(expressions,function(expression){
+function getIfStatements(expressions) {
+    return _.filter(expressions, function (expression) {
         return expression.type === esprima.Syntax.IfStatement;
     });
+}
+
+
+function Tree(){
+    this.root = new Node();
+}
+
+
+
+
+function Node(){
+    this.left = null;
+    this.right = null;
+}
+
+
+
+function processIfs(ifs) {
+
 }
