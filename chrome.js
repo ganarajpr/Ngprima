ad937f65670a4cfd091082c22aa0eee7d45c62ef
ganara.jpermunda <ganaraj.permunda@lab49.com>
2014-12-15 17:32:10 +0000

    pause resume cycle.

--- chrome.js
+++ chrome.js
@@ -1,17 +1,25 @@
 var Chrome = require('chrome-remote-interface');
 Chrome(function (chrome) {
     with (chrome) {
-        Timeline.start();
-        on('Timeline.eventRecorded', onExecutionContextCreated);
+        Debugger.enable();
+        on('Debugger.paused', function onDebuggerPaused(){
+            console.log(JSON.stringify(arguments));
+            chrome.Debugger.resume();
+        });
+        on('Debugger.resumed', function onDebuggerResumed(){
+            console.log(JSON.stringify(arguments));
+            chrome.Debugger.pause();
+        });
         Network.enable();
         Page.enable();
         Page.navigate({'url': 'http://localhost:8080'});
+        Debugger.pause();
+
+
+
     }
 }).on('error', function () {
     console.error('Cannot connect to Chrome');
 });
 
 
-function onExecutionContextCreated(){
-    console.log(JSON.stringify(arguments));
-}
