afb55bfc9e1c64546702630b493c617453bab192
ganaraj <ganarajpr@gmail.com>
2014-12-16 00:14:09 +0000

    stepping through

--- chrome.js
+++ chrome.js
@@ -1,19 +1,66 @@
 var Chrome = require('chrome-remote-interface');
 Chrome(function (chrome) {
     with (chrome) {
+        Runtime.enable();
+        /*on('Runtime.executionContextCreated',function(){
+            console.log(JSON.stringify(arguments));
+        });*/
+        var previousCallFrameId;
         Debugger.enable();
-        on('Debugger.paused', function onDebuggerPaused(){
-            console.log(JSON.stringify(arguments));
-            chrome.Debugger.resume();
+        Debugger.pause();
+        on('Debugger.paused', function onDebuggerPaused(response) {
+
+            /*chrome.Debugger.getBacktrace(null,function(a){
+             console.log(arguments);
+
+             });*/
+            //console.log(response);
+            if(response.callFrames[0].callFrameId !== previousCallFrameId){
+                previousCallFrameId = response.callFrames[0].callFrameId;
+                console.log(response.callFrames[0].functionName);
+
+                if(response.callFrames[0].scopeChain[0].object.className !== 'Window'){
+                    chrome.Runtime.getProperties({'objectId':response.callFrames[0].scopeChain[0].object.objectId},function(err,resp){
+                        resp.result.forEach(function(res){
+                            console.log(res.name);
+                        });
+                        chrome.Debugger.stepOver(null,function(){
+                            console.log(arguments);
+                        });
+                    });
+                }
+                else{
+                    //chrome.Debugger.resume();
+                    chrome.Debugger.stepOver(null,function(){
+                        console.log(arguments);
+                    });
+                }
+            }
+            else{
+                chrome.Debugger.stepOver(null,function(){
+                    console.log(arguments);
+                });
+            }
+
+
+
+            /*response.callFrames.forEach(function(callFrame){
+                callFrame.scopeChain.forEach(function(scope){
+                    chrome.Runtime.getProperties({'objectId':scope.object.objectId},function(err,resp){
+                        resp.result.forEach(function(res){
+                            console.log(res.name,'=',res.value);
+                        });
+                    });
+                });
+            });*/
+
+
         });
-        on('Debugger.resumed', function onDebuggerResumed(){
-            console.log(JSON.stringify(arguments));
-            chrome.Debugger.pause();
-        });
+
         Network.enable();
         Page.enable();
-        Page.navigate({'url': 'http://localhost:8080'});
-        Debugger.pause();
+        Page.navigate({'url': 'http://localhost:8080/test.html'});
+
 
 
 
